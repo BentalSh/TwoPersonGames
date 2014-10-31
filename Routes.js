3226700a224b64f4c0e98c8f01a28fd0fabca064
bakstansky <bakstansky@gmail.com>
2014-10-31 15:33:28 +0200

    Pusing to repository to check hosting options

---
+++ Routes.js
@@ -0,0 +1,28 @@
+/**
+ * Created by shahar on 31/10/14.
+ */
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000);
+
+app.set('view engine', 'jade');
+
+app.get('/index', function(req, res)
+{
+    res.render('index', {});
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg)
+    {
+        io.emit('chat message', msg);
+    });
+});
